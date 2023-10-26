760f6245605bb17d07a0bba80ccf3762774ae635
BhavyaTeja <bhavyateja5555@gmail.com>
2023-10-26 18:01:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+
+const express = require("express");
+const mongoose = require("mongoose");
+const studentRoute = require("./controller/studentRoute");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+mongoose.set("strictQuery",true); // to suppress the warning
+mongoose.connect("mongodb+srv://cloud:12345@cluster0.uodanuq.mongodb.net/schooldb");
+var db = mongoose.connection;
+db.on("open",()=>console.log("Connected to DB"));
+db.on("error",()=>console.log("Error occurred"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
+app.use("/studentRoute", studentRoute)
+app.listen(4000,()=>{
+    console.log("Server started at 4000");
+})
